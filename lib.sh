5280b9fc4f1dfee459f66887c116d1ec792c0f6b
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-09-16 14:24:11 +0300

    Documentation update, unreachable code removal

--- lib.sh
+++ lib.sh
@@ -399,7 +399,6 @@ EOF
 
     # launch backup
     virsh backup-begin "${VM_NAME}" --reuse-external --backupxml "${BACKUP_TASK_FILE}" || die "Failed to start backup for ${VM_NAME}"
-    [ "${?}" -ne 0 ] && die "virsh backup-begin ... failed, exit code ${?}"
 
     # wait completion (bounded by BACKUP_TIMEOUT_SECONDS)
     BACKUP_DEADLINE=$(( $(date +%s) + BACKUP_TIMEOUT_SECONDS ))
