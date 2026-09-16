1e68ccacf8c77511649dc834deb4eb0bc916248c
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-09-16 09:45:26 +0300

    bc.sh

--- bc.sh
+++ bc.sh
@@ -16,13 +16,30 @@
 #
 
 # ------------------------------------------------------------
+# Script/base dir
+# ------------------------------------------------------------
+get_base_dir() {
+    if [ -f "${0}" ]
+    then
+        # $0 points to a file on disk
+        printf '%s\n' "$(cd -- "$(dirname -- "${0}")" && pwd)"
+    else
+        # The script was sourced in a shell where $0 is not the script path
+        # Fallback to the current working directory
+        printf '%s\n' "$(pwd)"
+    fi
+}
+
+# ------------------------------------------------------------
 #  Main function to prevent occasional environment pollution
 # ------------------------------------------------------------
 closure() {
     #set -x # debug
 
+    BASE_DIR="$(get_base_dir)"
+
     # Load library
-    . "$(dirname -- "$(readlink -f -- "$0")")/lib.sh"
+    . "${BASE_DIR}/lib.sh"
 
     # Do the job
     environment
@@ -36,7 +53,7 @@ closure() {
 
     check_running
     create_running
-    
+
     create_current_backup_dir
     clean_obsolete_backups
     export_vm_and_disk_configuration
