8e7617b1b08e2544cf67eb2ee92cf6fcf8305e3e
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-09-18 13:30:12 +0300

    uid

--- testbed-builder.sh
+++ testbed-builder.sh
@@ -29,12 +29,6 @@ closure() {
     set -e
     #set -x # Debug
 
-    DISTRO=debian
-    if [ -n "${1}" ]
-    then
-        DISTRO="${1}"
-    fi
-
     # Define BASE_DIR
     BASE_DIR="$(get_base_dir)"
     # Load library
@@ -45,10 +39,56 @@ closure() {
     environment
     create_backup_dirs_and_log || die "Failed to create_backup_dirs_and_log"
 
-    cd "${BASE_DIR}/testbed-builder/${DISTRO}"
-    xorriso -as mkisofs -output "${BACKUP_DIR}/${DISTRO}"-seed.iso -volid cidata -joliet -rock user-data meta-data || die "Failed to create a ${BACKUP_DIR}/${DISTRO}-seed.iso"
+    DISTRO=debian
+    [ ! -z "${1}" ] && DISTRO="${1}"
+    [ "${DISTRO}" != "debian" ] && die "Distro ${1} is not supported at the moment"
 
-    unset BASE_DIR DISTRO
+    TARGET_VM_NAME="${DISTRO}-builder"
+    TARGET_DISK_FILE="${BACKUP_DIR}/${DISTRO}-builder.qcow2"
+    SEED_ISO_FILE="${BACKUP_DIR}/${DISTRO}"-seed.iso
+    GENERIC_IMAGE_URL="https://cloud.debian.org/images/cloud/trixie/latest/debian-13-genericcloud-amd64.qcow2"
+    GENERIC_IMAGE_FILE="${BACKUP_DIR}/${DISTRO}"-genericcloud.qcow2
+
+    cd "${BASE_DIR}/testbed-builder/${DISTRO}"
+    
+    if [ ! -f "${SEED_ISO_FILE}" ]
+    then
+        xorriso -as mkisofs -output "${SEED_ISO_FILE}" -volid cidata -joliet -rock user-data meta-data || die "Failed to create a ${BACKUP_DIR}/${DISTRO}-seed.iso"
+    fi
+
+    if [ ! -f "${GENERIC_IMAGE_FILE}" ]
+    then
+        curl -L -o "${GENERIC_IMAGE_FILE}" "${GENERIC_IMAGE_URL}"
+    fi
+    
+    if virsh list --all | grep -q "${TARGET_VM_NAME}"
+    then
+        if virsh list --all | grep -q "${TARGET_VM_NAME}" | grep -q "running"
+        then
+            virsh destroy "${TARGET_VM_NAME}" || die "Failed to destroy ${TARGET_VM_NAME}"
+        fi
+        virsh undefine "${TARGET_VM_NAME}" || die "Failed to undefine ${TARGET_VM_NAME}"
+        rm -rf "${TARGET_DISK_FILE}"
+    fi
+
+    if [ ! -f "${TARGET_DISK_FILE}" ]
+    then
+        qemu-img convert -O qcow2 -o compression_type=zstd -c "${GENERIC_IMAGE_FILE}" "${TARGET_DISK_FILE}" || die "Failed to convert ${GENERIC_IMAGE_FILE} to ${TARGET_DISK_FILE}"
+    fi
+
+    virt-install \
+      --name "${TARGET_VM_NAME}" \
+      --memory 4096 \
+      --vcpus 8 \
+      --cpu host-passthrough \
+      --disk path="${TARGET_DISK_FILE}",format=qcow2,bus=virtio \
+      --disk path="${SEED_ISO_FILE}",device=cdrom \
+      --network network=default,model=virtio \
+      --graphics vnc,listen=0.0.0.0 \
+      --os-variant debian13 \
+      --boot hd
+
+    unset BASE_DIR DISTRO TARGET_VM_NAME TARGET_DISK_FILE SEED_ISO_FILE GENERIC_IMAGE_URL GENERIC_IMAGE_FILE
 }
 
-closure
+closure "${@}"
