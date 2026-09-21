5e6c809ce2bf0c7a2b3c5ead8b7da01c1f29a834
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-09-21 11:17:54 +0300

    logs

--- testbed-builder.sh
+++ testbed-builder.sh
@@ -110,7 +110,7 @@ closure() {
       --cloud-init meta-data=${BASE_DIR}/testbed-builder/${DISTRO}/meta-data,user-data=${BASE_DIR}/testbed-builder/${DISTRO}/user-data
       #--disk path="${SEED_ISO_FILE}",device=cdrom
 
-      printf '%s\n' "Follow logs at /var/log/cloud-init-output.log"
+      printf '%s\n' "Follow logs via SSH: sudo cat /var/log/cloud-init-output.log | tail"
 
     unset BASE_DIR DISTRO TARGET_VM_NAME TARGET_DISK_FILE SEED_ISO_FILE QCOW2_URL QCOW2_FILE
 }
