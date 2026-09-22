b9e314c6aebb98ef122d257197bca0641b159010
Anatoly Shipov <anatoly.a.shipov@gmail.com>
2026-09-22 09:00:52 +0300

    alma enable crb

--- testbed-configurator.sh
+++ testbed-configurator.sh
@@ -31,10 +31,11 @@ EOF
 
 rhel_privileged() {
     sudo dnf -y upgrade
+    sudo dnf -y config-manager --enable crb
     sudo dnf -y install epel-release
     sudo dnf -y config-manager --disable epel-cisco-openh264
     sudo dnf -y install cronie git rsync acl sudo qemu-kvm libvirt virt-install cockpit mc tree curl
-    sudo dnf -y install xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-utils virt-manager xrdp openbox chromium firefox thunar xfce4-terminal xfce4-taskmanager mousepad dbus-daemon gvfs gvfs-smb weston #gvfs-sftp
+    sudo dnf -y install xorg-x11-server-Xorg virt-manager xrdp xorgxrdp openbox chromium firefox thunar xfce4-terminal xfce4-taskmanager mousepad dbus-daemon gvfs gvfs-smb weston #gvfs-sftp
     sudo dnf -y remove cloud-init
     sudo dnf -y clean all
     sudo systemctl enable --now crond
